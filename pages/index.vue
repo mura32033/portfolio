91ec8c846ea6b21ae8058b99aa8f21b18d37b1aa
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-09 20:08:32 +0900

    created index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial/>
+  <div class="wrapper">
+    <About />
+    <Works />
+    <Contact />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
 }
 </script>
+
+<style lang="scss">
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  margin: 0 10rem 1rem;
+}
+</style>
