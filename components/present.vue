22eb9a0e0be9bf604b9edd1549a4f65f9d819762
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-21 14:26:57 +0900

    card for presentation info

---
+++ components/present.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card awardCard">
+        <div class="awardTitle">{{data.title}}</div>
+        <div class="awardDetail">
+            <span class="awardDate">{{data.detail.date}}</span>
+            <span class="awardPlace">{{data.detail.place}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: Object
+    }
+}
+</script>
+
+<style lang="scss">
+.awardCard {
+    .awardTitle {
+        @apply text-2xl font-bold tracking-wider;
+    }
+    .awardContent {
+        @apply mt-2 my-auto;
+    }
+    .awardDetail {
+        @apply text-slate-500 text-sm text-right mt-4 ml-auto;
+        span {
+            @apply block;
+        }
+    }
+}
+</style>
