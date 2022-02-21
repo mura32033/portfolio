969560627d9aa1534ae3dea87160d2990c42f280
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-21 14:31:46 +0900

    arranged index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,45 @@
 <template>
-  <div class="wrapper">
-    
+  <div id="index">
+    <section>
+      <h2>こんにちは</h2>
+        <div class="sectionContent">
+          <img class="profileImg" src="https://mura32033.com/assets/img/selfie.png" alt="selfie">
+          <div class="contentGroup">
+            <Infodesc v-for="(i, index) in getAbout.info" :key="index" :data="i" />
+          </div>
+        </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-  layout: 'toppage',
-  name: 'IndexPage',
+    computed: {
+        ...mapGetters({
+            getAbout: 'json/getAbout',
+        }),
+    },
+    head() {
+        return{
+            title: "Hello, murasan world!"
+        }
+    }
 }
 </script>
 
 <style lang="scss">
-.wrapper{
-  display: flex;
-  flex-direction: column;
-  margin: 0 10rem 1rem;
+#index {
+  > section{
+    .profileImg {
+      @apply mr-0 md:mr-20 object-cover max-w-sm;
+    }
+    .sectionContent {
+      @apply flex flex-col md:flex-row items-center justify-center;
+      .contentGroup {
+        @apply flex flex-col mt-6 md:mt-0;
+      }
+    }
+  }
 }
 </style>
