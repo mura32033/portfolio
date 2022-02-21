38fd9cd7506b76e103648a69cb2ab99d8bf6eced
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-21 14:28:57 +0900

    card for other information

---
+++ components/infodesc.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="infoDesc">
+        <dt>{{data.title}}</dt>
+        <dd>{{data.content}} <template v-if="data.place">@{{data.place}}</template></dd>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: Object
+    }
+}
+</script>
+
+<style lang="scss">
+.infoDesc {
+    @apply flex flex-row mb-2 last:mb-0;
+    dt, dd {
+        @apply py-2 px-4 rounded;
+    }
+    dt {
+        @apply bg-teal-600 text-white;
+    }
+}
+</style>
