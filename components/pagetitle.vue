158a105852215437ee55dc541834e49c76e791a0
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-21 14:27:32 +0900

    pagetitle component for all page

---
+++ components/pagetitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="pagetitle">
+        <h1><slot name="title" /></h1>
+        <div class="pageDesc" v-if="slotted">
+            <slot name="desc" />
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    computed: {
+        slotted() {
+            return !!this.$slots.desc
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.pagetitle {
+    @apply flex flex-col justify-center mt-12;
+    h1 {
+        @apply mb-8;
+    }
+}
+</style>
