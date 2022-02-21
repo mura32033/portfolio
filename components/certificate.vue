199bd564e03e68e3557f7fb9f304ef269e3e8917
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-21 14:26:33 +0900

    card for certification info

---
+++ components/certificate.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card certificateCard">
+        <div class="certificateTitle">{{data.title}}</div>
+        <div class="certificateContent">{{data.content}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        data: Object
+    }
+}
+</script>
+
+<style lang="scss">
+.certificateCard {
+    .certificateTitle {
+        @apply text-2xl font-bold tracking-wider;
+    }
+    .certificateContent {
+        @apply mt-2 my-auto;
+    }
+}
+</style>
