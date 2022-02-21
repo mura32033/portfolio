72b402789a24d5f586b11f8c6979a3ab0ed3a174
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-21 14:25:27 +0900

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./components/**/*.vue",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.js",
+    "./nuxt.config.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['NotoSans', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
