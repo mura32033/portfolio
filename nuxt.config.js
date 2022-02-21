520be03ac9bbd9f1d34bd8bfdb6606e7de8eaf6f
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-21 15:16:29 +0900

    updated nuxt generate setting for nuxt content

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,4 +60,13 @@ export default {
     }
   },
 
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content().only(['path']).fetch()
+
+      return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  }
+
 }
