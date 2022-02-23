4406790baeb8d1ec88be26fa7f03ca4e6ee2154b
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-23 14:15:22 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  mode: 'universal',
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'むらさん',
