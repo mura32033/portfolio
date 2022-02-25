e75d784e35de1039ac21973d818f3bc436fcd7e9
mura32033 <mura3-2033@hotmail.co.jp>
2022-02-26 01:22:56 +0900

    順番変えた

--- components/navmenu.vue
+++ components/navmenu.vue
@@ -2,8 +2,8 @@
     <div class="nav">
         <NuxtLink to="/about">About</NuxtLink>
         <NuxtLink to="/works">Works</NuxtLink>
-        <NuxtLink to="/contact">Contact</NuxtLink>
         <NuxtLink to="/docs">Docs</NuxtLink>
+        <NuxtLink to="/contact">Contact</NuxtLink>
     </div>
 </template>
 
@@ -14,7 +14,7 @@
         @apply block py-2 px-8 md:-mb-1 md:hover:rounded-t-md md:hover:bg-emerald-400 md:hover:font-bold;
         min-width: 64px;
         &.nuxt-link-active {
-            @apply font-bold rounded-tr-full rounded-br-full md:rounded-t-md md:rounded-tr-none md:rounded-br-none bg-emerald-400;
+            @apply font-bold rounded-tr-full rounded-br-full md:rounded-t-md md:rounded-br-none bg-emerald-400;
         }
     }
 }
