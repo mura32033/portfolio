bd5be204eacc6e9c6aadd067d1619ae5eadd90a8
mura32033 <mura3-2033@hotmail.co.jp>
2022-09-29 15:03:58 +0900

    created document card and card group for apperance

---
+++ components/DocsCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <nuxt-link
+    :to="path"
+    class="
+      flex flex-col
+      justify-center
+      overflow-hidden
+      border border-gray-300
+      dark:border-gray-700
+      rounded-lg
+      hover:border-gray-700
+      dark:hover:border-gray-300
+      hover:shadow-lg
+      dark:hover:bg-gray-800
+    "
+  >
+    <div class="p-8">
+      <div class="text-2xl font-medium">{{ title }}</div>
+      <p class="mt-2 text-sm text-gray-400">最終更新日時: {{ date }}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -6,20 +6,17 @@
                 <p>備忘録などなど。気が向いたときに更新していきます。</p>
             </template>
         </Pagetitle>
-        <ul>
-            <li v-for="l in list" :key="l.slug">
-                <nuxt-link :to="l.path">
-                    {{ l.title }}
-                </nuxt-link>
-            </li>
-        </ul>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <DocsCard v-for="l in list" :key="l.slug" :title="l.title" :path="l.path" :date="formatDate(l.updatedAt)" />
+        </div>
     </div>
 </template>
 
 <script>
+const dayjs = require('dayjs')
 export default {
     async asyncData ({ $content }) {
-        const list = await $content('docs', { deep: true }).only(['title', 'slug', 'path']).where({ isPublished: true }).sortBy('updatedAt', 'desc').fetch()
+        const list = await $content('docs', { deep: true }).only(['title', 'slug', 'path', 'updatedAt']).where({ isPublished: true }).sortBy('updatedAt', 'desc').fetch()
         return {
             list
         }
@@ -28,6 +25,16 @@ export default {
     head() {
         return{
             title: 'Docs'
+        }
+    },
+    mounted() {
+        dayjs.extend(require('dayjs/plugin/utc'))
+        dayjs.extend(require('dayjs/plugin/timezone'))
+        dayjs.tz.setDefault('Asia/Tokyo')
+    },
+    methods: {
+        formatDate(date) {
+            return dayjs(date).format('YYYY/MM/DD HH:mm:ss')
         }
     }
 }
